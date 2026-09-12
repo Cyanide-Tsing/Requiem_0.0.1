9295a368dad428b090cc255be3217ffb18699fbc
Cyanide-Tsing <zno2_1cl1hindole@163.com>
2026-09-13 05:04:42 +0800

    soul fragment uses add and rp soul sand

--- server_scripts/requiem/recipes/soul_fragment_replace.js
+++ server_scripts/requiem/recipes/soul_fragment_replace.js
@@ -1,4 +1,18 @@
 ServerEvents.recipes(event=>{
     event.remove({id:'create:haunting/soul_campfire'})
     event.shaped('minecraft:soul_campfire', [' S ', 'SFS', 'LLL'], {S:'minecraft:stick', F:'requiem:soul_fragment', L:'#minecraft:logs'}).id('minecraft:soul_campfire')
+    event.shaped('4x minecraft:soul_torch', ['F  ', 'S  ', '   '], {S:'minecraft:stick', F:'requiem:soul_fragment'}).id('minecraft:soul_torch')
+
+    event.replaceInput({id:"goety:totem_of_souls"}, 'minecraft:soul_lantern', 'requiem:soul_fragment')
+    event.replaceInput({id:"goety:totem_of_roots"}, 'goety:ectoplasm', 'requiem:soul_fragment')
+    event.replaceInput({id:"goety:totem_replenish"}, 'minecraft:soul_lantern', 'requiem:soul_fragment')
+
+    event.recipes.goety.ritual('requiem:soulmeter', 'goety:craft', ['goety:cursed_ingot','goety:cursed_ingot','goety:cursed_ingot','goety:cursed_ingot'])
+        .craftType('forge')
+        .activationItem('requiem:soul_fragment')
+        .duration(10)
+        .soulCost(10)
+        .id("requiem:rituals/forge/soulmeter")
+    
+    event.shaped('requiem:soulmeter', [' L ', 'LFL', ' L '], {L:'kubejs:lead_ingot', F:'requiem:soul_fragment'}).id('requiem:soulmeter')
 })
