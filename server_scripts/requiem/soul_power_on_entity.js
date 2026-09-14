e7c7b7ca8694f64fa8cefa5a91cc34d67693eb2e
Cyanide-Tsing <zno2_1cl1hindole@163.com>
2026-09-15 05:31:59 +0800

    use attribute to fit hp with sp, revives noAI mob

--- server_scripts/requiem/soul_power_on_entity.js
+++ server_scripts/requiem/soul_power_on_entity.js
@@ -6,15 +6,23 @@
     10% summon necromacer instead of zombie when sp > 100000
 */
 
+const $Attributes = Java.loadClass('net.minecraft.world.entity.ai.attributes.Attributes')
+
 EntityEvents.spawned(event=>{
     if(!event.entity.isLiving()) return;
     let sp = event.server.persistentData.getInt("soul_power")
     let factor = Math.pow(1.01, (sp - 50000) / 100)
     let hp = event.entity.getMaxHealth() * factor
-    event.entity.setMaxHealth(hp)
-    event.entity.setHealth(hp)
-    if(sp < 1000){
-        event.entity.nbt.merge("{NoAI:1.0b}") // Working on removing AI when SP too low. Commands success
-        event.server.getPlayerList().getPlayers().forEach(player=>player.tell('noAI operation success'))
+    event.entity.setAttributeBaseValue($Attributes.MAX_HEALTH, hp)
+    event.entity.heal(hp)
+    if(sp < 1000)event.entity.mergeNbt({ NoAI: true }) // oh shit WHAT IS IT
+})
+
+ItemEvents.entityInteracted(event=>{
+    const {player} = event
+    let entity = event.getTarget()
+    if(player.mainHandItem.id == 'requiem:soul_fragment' && entity.getNbt().getBoolean("NoAI") && player.isCrouching()){
+        entity.mergeNbt({ NoAI: false })
+        player.mainHandItem.shrink(1)
     }
 })
